de721df615a0e4d4a154cd302a003a3205eaa8b4
sahjikutty-dot <sahjikutty@gmail.com>
2026-04-09 19:05:51 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
+import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
-import fitz  # PyMuPDF
+import fitz
 import re
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
